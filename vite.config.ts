39809463372236ce5d713a55492a04b84871804d
chungee <xu.zhou@effem.com>
2022-07-15 17:21:42 +0800

    build: :wrench: add bulid

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 const path = require('path');
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -21,4 +24,22 @@ export default defineConfig({
       reporter: ['text', 'json', 'html'],
     },
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.tsx'),
+      name: 'Danger',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+
+  },
+
 })
