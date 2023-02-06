e0853d61ce5eaf26b0156b076f7eaeb3608cdc00
chungee <xu.zhou@effem.com>
2023-02-06 16:37:50 +0800

    fix: alias not working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const { loadConfigFromFile, mergeConfig } = require("vite");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -19,5 +22,17 @@ module.exports = {
   "features": {
     "storyStoreV7": true,
     "interactionsDebugger": true,
-  }
+  },
+  //fix alias not working  https://github.com/storybookjs/builder-vite/issues/85
+  viteFinal: async (config) => {
+    const { config: userConfig } = await loadConfigFromFile(
+        path.resolve(__dirname, "../vite.config.ts")
+    );
+
+    return mergeConfig(config, {
+      ...userConfig,
+      // manually specify plugins to avoid conflict
+      plugins: [],
+    });
+  },
 }
